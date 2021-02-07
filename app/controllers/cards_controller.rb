b78c8e0ddd066ee4bd085dcdccbed15143ed16b4
greenh <violetacastrogutierrez@gmail.com>
2021-02-07 17:08:53 +0000

    clean up code

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,14 +3,14 @@ class CardsController < ApplicationController
   def create
     card = Card.new(limit: params[:limit])
     if card.save
-      render json: { card_id: card.id, limit: card.limit, error: false }, status: 201
+      render json: { card_id: card.id, limit: card.limit, error: false },
+             status: 201
     else
       render json: { error: "Limit must be greater or equal to $100
                      and less than $1,000,000" }, status: 400
     end
   end
 
-  # check for the existence of card in the database and return findings
   def get_card_if_exists
     card = Card.first
     info = card.attributes.merge({total_usage: card.sum_of_charges}) if card
