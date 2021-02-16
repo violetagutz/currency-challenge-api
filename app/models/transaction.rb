3fd4cef94c9a4ced61b7428d8f88f6db1c37d2ff
greenh <violetacastrogutierrez@gmail.com>
2021-02-16 19:55:44 +0000

    check if theres duplicates in the array of transactions grouped by amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,5 +5,42 @@ class Transaction < ApplicationRecord
   validates :amount, :card_id, presence: true
   validates :amount, numericality: { only_integer: true, greater_than: 0 }
 
-  enum state: { "pending": 0 }
+  enum state: { "pending": 0, "declined": 1 }
+
+  def self.group_transactions_by_amount
+    transactions_by_amount = {}
+
+    self.where(state: "pending").each do |transaction|
+      # getting the amount out of transaction
+      amount = transaction[:amount]
+      # set an array as a value of the hash with the key of amount
+      transactions_array = transactions_by_amount[amount]
+      # if array has values in it, add the transaction to it
+      if transactions_array
+        transactions_array.push(transaction)
+        # updating the hash with the new value in the array
+        transactions_by_amount[amount] = transactions_array
+      else
+        # create a new array
+        transactions_array = []
+        # add the transaction to it as the first value
+        transactions_array.push(transaction)
+        # update the hash value with the new element in the array
+        transactions_by_amount[amount] = transactions_array
+      end
+    end
+    return transactions_by_amount
+  end
+
+  def self.check_duplicates
+    transactions_by_amount = self.group_transactions_by_amount
+    binding.pry
+    return
+  end
+
+  def self.verify_pending_transactions
+    self.check_duplicates
+  end
+
 end
+
